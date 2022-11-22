cf0b8802dbd741011bd6815e78f1f6aa828f9f31
weiwang127 <weiwang127@iflytek.com>
2022-11-22 22:22:38 +0800

    ELK实现日志收集代码提交

--- mall-admin/src/main/java/com/ww/mall/controller/PmsProductController.java
+++ mall-admin/src/main/java/com/ww/mall/controller/PmsProductController.java
@@ -1,9 +1,12 @@
 package com.ww.mall.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.ww.mall.base.CommonResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,10 +17,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/product")
 public class PmsProductController {
 
+    private Logger logger = LoggerFactory.getLogger(PmsProductController.class);
+
     @ApiOperation("查询商品")
     @GetMapping(value = "/list")
     public CommonResult<String> getList() {
-        System.out.println("进入了查询商品方法...");
+        logger.info("查询商品入参【{}】", "查询到商品....");
         return CommonResult.success("success");
     }
 
